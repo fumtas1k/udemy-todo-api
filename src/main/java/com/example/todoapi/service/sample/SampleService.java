3350e15bfc5bcca189f3be9e2725a7f2d0d1fb5a
fumtas1k <fumta01a7@gmail.com>
2022-11-22 21:54:34 +0900

    [add]エンティティとサービス追加

---
+++ src/main/java/com/example/todoapi/service/sample/SampleService.java
@@ -0,0 +1,11 @@
+package com.example.todoapi.service.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+  public SampleEntity find() {
+    return new SampleEntity("hello");
+  }
+}
