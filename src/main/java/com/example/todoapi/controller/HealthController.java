8a71dfe19a1650f181b3ad850fef4d83099d31a8
fumtas1k <fumta01a7@gmail.com>
2022-11-22 20:39:15 +0900

    [add]HealthController追加

--- src/main/java/com/example/todoapi/controller/HealthController.java
+++ src/main/java/com/example/todoapi/controller/HealthController.java
@@ -1,3 +1,20 @@
 package com.example.todoapi.controller;
 
-public class HealthController implements HealthApi {}
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.Optional;
+
+@RestController
+public class HealthController implements HealthApi {
+  @Override
+  public Optional<NativeWebRequest> getRequest() {
+    return HealthApi.super.getRequest();
+  }
+
+  @Override
+  public ResponseEntity<Void> healthGet() {
+    return ResponseEntity.ok().build();
+  }
+}
