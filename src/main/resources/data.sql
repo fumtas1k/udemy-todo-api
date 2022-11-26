f39cf2022be618941135e9e8729c6bc03201abea
fumtas1k <fumta01a7@gmail.com>
2022-11-26 15:10:35 +0900

    [add]タスクテーブルとデータ追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,10 @@
 insert into samples (content) values
 ('hello, world!!'),
 ('good morning!!');
+
+insert into tasks (title) values
+('タスク1'),
+('タスク2'),
+('タスク3'),
+('タスク4'),
+('タスク5');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,11 @@ create table if not exists samples (
   `updated_at` datetime not null default current_timestamp on update current_timestamp comment '更新日時',
   primary key (`id`)
 ) comment 'サンプルテーブル';
+
+create table if not exists tasks (
+  `id` bigint unsigned not null auto_increment comment 'タスクID',
+  `title` varchar(256) not null comment 'タスクのタイトル',
+  `created_at` datetime not null default current_timestamp comment '作成日時',
+  `updated_at` datetime not null default current_timestamp on update current_timestamp comment '更新日時',
+  primary key(`id`)
+) comment 'タスク';
