4b8bd889b4fcf4d3b492160652dc9c633cc87adf
fumtas1k <fumta01a7@gmail.com>
2022-11-26 15:56:55 +0900

    [change]検索メソッドにタスクリポジトリを使用

--- src/main/java/com/example/todoapi/service/task/TaskService.java
+++ src/main/java/com/example/todoapi/service/task/TaskService.java
@@ -1,11 +1,20 @@
 package com.example.todoapi.service.task;
 
+import com.example.todoapi.repository.task.TaskRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TaskService {
-  public TaskEntity find() {
 
-    return new TaskEntity(2L, "from Service");
+  private final TaskRepository taskRepository;
+
+  public TaskEntity find(Long taskId) {
+
+    return taskRepository
+        .select(taskId)
+        .map(record -> new TaskEntity(record.getId(), record.getTitle()))
+        .orElseThrow(() -> new IllegalStateException("id: " + taskId + "のタスクは存在しません!"));
   }
 }
