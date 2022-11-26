11747941861e86615eed00b5efe2081b140a8a26
fumtas1k <fumta01a7@gmail.com>
2022-11-26 15:57:31 +0900

    [change]タスクサービスを使用

--- src/main/java/com/example/todoapi/controller/task/TaskController.java
+++ src/main/java/com/example/todoapi/controller/task/TaskController.java
@@ -2,17 +2,23 @@ package com.example.todoapi.controller.task;
 
 import com.example.todoapi.controller.TasksApi;
 import com.example.todoapi.model.TaskDTO;
+import com.example.todoapi.service.task.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class TaskController implements TasksApi {
 
+  private final TaskService taskService;
+
   @Override
-  public ResponseEntity<TaskDTO> showTask() {
+  public ResponseEntity<TaskDTO> showTask(Long taskId) {
+    var entity = taskService.find(taskId);
     var body = new TaskDTO();
-    body.setId(1L);
-    body.setTitle("Test");
+    body.setId(entity.getId());
+    body.setTitle(entity.getTitle());
     return ResponseEntity.ok(body);
   }
 }
